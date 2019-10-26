78880dfef3c0130ef3bc4e7ee644b3d68bda6318
Said Gamih <said.gamih@gmail.com>
2019-10-26 15:45:39 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ $("#close-alertbox").click(function() {
 $("#email-form").submit(function(event) {
   event.preventDefault();
   $.post(
-    "https://saysu.washandrenov.com/email/",
+    "https://saysu.washandrenov.com/email/index.php",
     $("#email-form").serialize()
   )
     .done(function(data) {
